02e45365fe9e46544343c2a1d76526ca1f7a22ba
pandutibil <panduranga.varma@tibilsolutions.com>
2022-07-01 15:56:24 +0530

    State wise etb data response has changed

--- back-end/controller/etb/etbController.js
+++ back-end/controller/etb/etbController.js
@@ -1,14 +1,54 @@
 const path = require('path');
 const fs = require('fs');
 
-exports.getStateWiseETBCoverageData = (req, res, next) => {
+exports.getETBMetrics = (req, res, next) => {
 	return new Promise(async function (resolve, reject) {
-        let stateWiseETBCoverageData = require(path.join(__dataFolderPath, 'etb/StateWiseETBCoverageData.json'));
+        let ETBMetrics = require(path.join(__dataFolderPath, 'etb/ETBMetrics.json'));
 
 		try {
 			res.status(200).send({
 				status: 200,
-				result: stateWiseETBCoverageData
+				result: ETBMetrics
+			})
+		} catch (error) {
+			res.send({
+				status: error.status || 500,
+				message: error.message || "Internal server error",
+				errorObject: error
+			});
+		}
+	});
+}
+
+exports.getStateWiseETBCoverageData = (req, res, next) => {
+	return new Promise(async function (resolve, reject) {
+        let stateWiseETBCoverageData = require(path.join(__dataFolderPath, 'etb/StateWiseETBCoverageData.json'));
+		let columns = [
+			{
+				name: 'State Name',
+				property: 'State Name'
+			},
+			{
+				name: 'Total Physical',
+				property: 'Total Physical textbooks excluding adopted (Curriculum+Supplementary)'
+			},
+			{
+				name: 'State energised (ETB)',
+				property: 'State energised (ETB)'
+			},
+			{
+				name: 'ETB Coverage',
+				property: 'ETB Coverage'
+			}
+		];
+
+		try {
+			res.status(200).send({
+				status: 200,
+				result: {
+					columns,
+					data: stateWiseETBCoverageData
+				}
 			})
 		} catch (error) {
 			res.send({
