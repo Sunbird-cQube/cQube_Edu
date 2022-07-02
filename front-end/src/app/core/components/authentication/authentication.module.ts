132f75cdcec5df5f268eaf90b1193a97fd928fc5
FaizanMohammed326 <faizan.mohammed@tibilsoultions.com>
2022-07-02 15:04:33 +0530

    authentication module with login component added

---
+++ front-end/src/app/core/components/authentication/authentication.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './pages/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AuthenticationComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ]
+})
+export class AuthenticationModule { }
