cd72ccc9e1ed779bba66072b9b64503af30825a2
pandutibil <panduranga.varma@tibilsolutions.com>
2022-07-04 14:02:37 +0530

    Map zoom modifications and etb QR Coverage tab enabled and gauge chart configuration done

--- front-end/src/app/shared/components/charts/gauge-chart/gauge-chart.component.ts
+++ front-end/src/app/shared/components/charts/gauge-chart/gauge-chart.component.ts
@@ -17,9 +17,11 @@ export class GaugeChartComponent implements OnInit, AfterViewInit {
   chart!: Highcharts.Chart;
   @Input() height = 350;
   @Input() title!: string;
-  @Input() categories!: string[];
-  @Input() series!: any[];
-  @Input() gridLineColor: string = '#aaa';
+  @Input() set options(options: Highcharts.Options | undefined) {
+    if (options) {
+        this.createBarChart(options);
+    }
+  }
 
   @ViewChild('container') container: any;
 
@@ -29,85 +31,70 @@ export class GaugeChartComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.createBarChart();
   }
 
-  createBarChart(): void {
-    let ref: GaugeChartComponent = this;
-    this.chart = Highcharts.chart(this.container.nativeElement, {
-      chart: {
-        type: 'solidgauge'
-      },
-      title: {
-        text: ""
-      },
-      pane: {
-          center: ['50%', '80%'],
-          size: '100%',
-          startAngle: -90,
-          endAngle: 90,
-          background: [{
-              backgroundColor: { linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 }, stops: [[0, '#ffffff'], [1, '#e6e6e6']] },
-              innerRadius: '80%',
-              outerRadius: '100%',
-              shape: 'arc'
-          }]
-      },
-      exporting: {
-          enabled: false
-      },
-      tooltip: {
-          enabled: false
-      },
-      // the value axis
-      yAxis: {
-          stops: [
-              [0.9, '#DF5353'],// red
-              [0.5, '#DDDF0D'],// yellow
-              [0.1, '#55BF3B'], // green
-          ],
-          lineWidth: 0,
-          tickWidth: 0,
-          minorTickInterval: null,
-          tickAmount: 2,
-          title: {
-              y: 85,
-              text: 'Overall ETB coverage'
-          },
-          labels: {
-              y: 16
-          },
-          min: 0,
-          max: 100
-      },
-      plotOptions: {
-          solidgauge: {
-              dataLabels: {
-                  y: 5,
-                  borderWidth: 0,
-                  useHTML: true
-              }
-          }
-      },
-      credits: {
-          enabled: false
-      },
-      series: [{
-          type: 'solidgauge',
-          name: 'Speed',
-          data: [60.6],
-          innerRadius: '80%',
-          dataLabels: {
-              format:
-                  '<div style="text-align:center">' +
-                  '<span style="font-size:25px">{y}%</span><br/>' +
-                  '</div>'
-          },
-          tooltip: {
-              valueSuffix: ' %'
-          }
-      }]
-    },  function(this: any) {
+  createBarChart(options: Highcharts.Options): void {
+    let defaultOptions: Highcharts.Options = {
+        chart: {
+          type: 'solidgauge'
+        },
+        title: {
+          text: ""
+        },
+        pane: {
+            center: ['50%', '80%'],
+            size: '100%',
+            startAngle: -90,
+            endAngle: 90,
+            background: [{
+                backgroundColor: { linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 }, stops: [[0, '#ffffff'], [1, '#e6e6e6']] },
+                innerRadius: '80%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }]
+        },
+        exporting: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.9, '#DF5353'],// red
+                [0.5, '#DDDF0D'],// yellow
+                [0.1, '#55BF3B'], // green
+            ],
+            lineWidth: 0,
+            tickWidth: 0,
+            minorTickInterval: null,
+            tickAmount: 2,
+            title: {
+                y: 85,
+                text: ''
+            },
+            labels: {
+                y: 16
+            },
+            min: 0,
+            max: 100
+        },
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: []
+    };
+    this.chart = Highcharts.chart(this.container.nativeElement, Highcharts.merge(defaultOptions, options),  function(this: any) {
 
     });
   }
--- front-end/src/app/views/nishtha/pages/dashboard/dashboard.component.ts
+++ front-end/src/app/views/nishtha/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { IDashboardMenu } from 'src/app/core/models/IDashboardCard';
 import { NishthaService } from 'src/app/core/services/nishtha/nishtha.service';
 
@@ -11,13 +13,19 @@ export class DashboardComponent implements OnInit {
 
   nishthaMenu: IDashboardMenu[] | undefined;
 
-  constructor(private readonly _nishthaService: NishthaService) {
+  constructor(private readonly _nishthaService: NishthaService, private readonly _router: Router) {
     this._nishthaService.getNishthaMenu().subscribe(nishthaMenuRes => {
       this.nishthaMenu = nishthaMenuRes.result;
     })
   }
 
   ngOnInit(): void {
+  }
+
+  onClickOfDashboardItem(cardInfo: IDashboardMenu | undefined): void {
+    if (cardInfo) {
+      this._router.navigate([cardInfo.navigationURL])
+    }
   }
 
 }
