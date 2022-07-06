a9b461b19a707a0a4b808b403569c24257a4a711
tibil-it <67742511+tibil-it@users.noreply.github.com>
2022-07-06 13:02:44 +0530

    Revert "card image size issue fix"

--- front-end/src/app/shared/components/cards/dashboard-card/dashboard-card.component.ts
+++ front-end/src/app/shared/components/cards/dashboard-card/dashboard-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IDashboardMenu } from 'src/app/core/models/IDashboardCard';
 
 @Component({
@@ -7,22 +7,13 @@ import { IDashboardMenu } from 'src/app/core/models/IDashboardCard';
   styleUrls: ['./dashboard-card.component.scss']
 })
 export class DashboardCardComponent implements OnInit {
-  isTextWrapIssueFix: boolean  = false;
+
   @Input() cardInfo: IDashboardMenu | undefined;
   @Output() onClick: EventEmitter<IDashboardMenu | undefined> = new EventEmitter<IDashboardMenu | undefined>();
 
   constructor() { }
-  //Listen when browser is zoomed and add effects to text overLapping issues.
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-   if(window.innerWidth <= 1615) {
-      this.isTextWrapIssueFix = true;
-   } 
-}
 
   ngOnInit(): void {
-    // onLoad with browser ZoomIn will add effects to text overLapping issues.
-    this.onResize(event);
   }
 
   len(arr: any[]){
