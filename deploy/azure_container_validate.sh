3eb5304c50ff1f0f4a92f92233d12df15e05feb1
asifbasha61 <asif.abdul@tibilsolutions.com>
2022-07-13 15:22:12 +0530

    azure validations changed

--- deploy/azure_container_validate.sh
+++ deploy/azure_container_validate.sh
@@ -14,35 +14,25 @@ else
 fi
 }
 
-check_az_key(){
-    az_key_status=0
-    export AZURE_STORAGE_CONNECTION_STRING="$1"
-
-    az storage container list --account-name $azure_account_name --account-key $azure_account_key > /dev/null 2>&1
-    if [ ! $? -eq 0 ]; then echo "Error - Invalid az account key"; fail=1
-        az_key_status=1
-    fi
-}
-
-check_az_storage_account_name(){
+check_az_storage_connection_string(){
     az_account_status=0
     export AZURE_STORAGE_CONNECTION_STRING="$1"
 
-    az storage container list --account-key $azure_account_key --account-name $azure_account_name > /dev/null 2>&1
-    if [ ! $? -eq 0 ]; then echo "Error - Invalid az account name"; fail=1
+    az storage container list  --connection-string "$azure_storage_connection_string" > /dev/null 2>&1
+    if [ ! $? -eq 0 ]; then echo "Error - Invalid az storage connection string"; fail=1
         az_account_status=1
     fi
 }
 
-
 check_az_container(){
   az_container_status=0
   export AZURE_STORAGE_CONNECTION_STRING="$1"
-  containerstatus=$(az storage container exists --account-name $azure_account_name --account-key $azure_account_key --name $2 | grep -c false )
+  containerstatus=$(az storage container exists --connection-string "$azure_storage_connection_string" --name $2 | grep -c false )
     if [  $containerstatus == 1 ]; then echo "Error - [ $1 : $2 ] Container not owned or not found. Please change the container name in azure_container_config.yml"; fail=1
          az_container_status=1
     fi
 }
+
 
 get_azure_container_config_values(){
 key=$1
@@ -62,14 +52,13 @@ echo -e "\e[0;33m${bold}Validating the azure_container_config file...${normal}"
 
 
 # An array of mandatory values
-declare -a arr=("azure_account_name" "azure_account_key" "azure_input_container" "azure_output_container")
+declare -a arr=("azure_input_container" "azure_output_container" "azure_storage_connection_string")
 
 # Create and empty array which will store the key and value pair from config file
 declare -A vals
 
 # Getting aws keys
-azure_account_name=$(awk ''/^azure_account_name:' /{ if ($2 !~ /#.*/) {print $2}}' azure_container_config.yml)
-azure_account_key=$(awk ''/^azure_account_key:' /{ if ($2 !~ /#.*/) {print $2}}' azure_container_config.yml)
+azure_storage_connection_string=$(awk ''/^azure_storage_connection_string:' /{ if ($2 !~ /#.*/) {print $2}}' azure_container_config.yml)
 
 # Iterate the array and retrieve values for mandatory fields from config file
 for i in ${arr[@]}
@@ -83,18 +72,11 @@ key=$i
 value=${vals[$key]}
 case $key in
 
-   azure_account_name)
-       if [[ $value == "" ]]; then
-          echo "Error - in $key. Unable to get the value. Please check."; fail=1
-	   else
-		   check_az_storage_account_name $key $value 	  
-       fi
-       ;;
-   azure_account_key)
+   azure_storage_connection_string)
        if [[ $value == "" ]]; then
           echo "Error - in $key. Unable to get the value. Please check."; fail=1
       else
-          check_az_key $key $value
+          check_az_storage_connection_string $key $value
        fi
        ;;
    azure_input_container)
