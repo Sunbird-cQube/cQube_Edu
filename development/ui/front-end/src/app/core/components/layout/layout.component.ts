c3b9844f82b89991427ef85a40c0656b735d6c67
Sharath Prasad <sharath@Sharaths-MacBook-Pro.local>
2022-07-13 17:41:42 +0530

    Issue #SB-0000:Cqube Placeholder creation

--- development/ui/front-end/src/app/core/components/layout/layout.component.ts
+++ development/ui/front-end/src/app/core/components/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ConfigService } from 'src/app/core/services/config/config.service';
+import { IDashboardMenu } from '../../models/IDashboardCard';
 import { IMenuItem } from '../../models/IMenuItem';
 
 @Component({
@@ -12,8 +13,25 @@ export class LayoutComponent implements OnInit {
   menu: IMenuItem[] | undefined;
 
   constructor(private readonly _configService: ConfigService) {
-    this._configService.getMenu().subscribe(menuResult => {
-      this.menu = menuResult.result;
+    this._configService.getDashboardMenu().subscribe(menuResult => {
+      console.log(menuResult);
+      this.menu = [];
+      let menuToDisplay: IMenuItem | any = {};
+      menuToDisplay.label = "Dashboard";
+      menuToDisplay.path = "/dashboard";
+      menuToDisplay.icon = '';
+      menuToDisplay.isSelected = true;
+      menuToDisplay.basepath = "dasboard";
+      this.menu.push(menuToDisplay);
+      menuResult.result.forEach((dasboardMenu:IDashboardMenu)=>{
+        let menuToDisplay: IMenuItem | any = {};
+        menuToDisplay.label = dasboardMenu.title;
+        menuToDisplay.path = dasboardMenu.navigationURL;
+        menuToDisplay.icon = dasboardMenu.icon;
+        menuToDisplay.isSelected = false;
+        this.menu?.push(menuToDisplay);
+      });
+      //this.menu = menuResult.result;
     });
   }
 
