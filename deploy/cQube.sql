04822d01cf191abe549acdd9087c10ef45887b30
asifbasha61 <asif.abdul@tibilsolutions.com>
2022-07-15 11:20:16 +0530

    sql file updated

--- deploy/cQube.sql
+++ deploy/cQube.sql
@@ -2,7 +2,7 @@ create table if not exists  users(
 user_id serial primary key not null,
 user_name varchar(20),
 email_id varchar(50),
-pasword varchar(40),
+password varchar(40),
 is_first_login bit,
 state_id bigint,
 created_date timestamp with time zone default now(),
