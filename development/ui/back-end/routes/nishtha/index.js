7bd6f18610c3b51551d66ec5a8fb2e05fe527a7e
EXTERNAL Ankani Pandu Ranga Varma (Tibil , SX/EHC35) <external.PanduRangaVarma.Ankani@in.bosch.com>
2022-07-22 15:40:22 +0530

    Code cleanup

--- development/ui/back-end/routes/nishtha/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const nishthaController = require('../../controller/nishtha/NishthaController');
-
-router.get("/getStateWiseEnrollmentData/:version", nishthaController.getStateWiseEnrollmentData);
-
-module.exports = router;
