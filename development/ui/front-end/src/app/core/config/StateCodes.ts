27399722f44c7e09adc15a2bc3fdccdfe1ea6dae
atulmane01 <atul.mane@tibilsolutions.com>
2022-07-26 21:05:10 +0530

    added state name in login page

--- development/ui/front-end/src/app/core/config/StateCodes.ts
+++ development/ui/front-end/src/app/core/config/StateCodes.ts
@@ -38,3 +38,59 @@ export const StateCodes = [
     "UT",
     "WB"
 ]
+
+export const stateNames = [
+    {
+        stateName: 'Andaman and Nicobar Islands',
+        stateCode: 'AN'
+    },
+    {
+        stateName: 'Andhra Pradesh',
+        stateCode: 'AP'
+    },
+    {
+        stateName: 'Arunachal Pradesh',
+        stateCode: 'AR'
+    },
+    {
+        stateName: 'Assam',
+        stateCode: 'AS'
+    },
+    ,
+    {
+        stateName: 'Bihar',
+        stateCode: 'BR'
+    },
+    {
+        stateName: 'Chandigarh',
+        stateCode: 'CH'
+    },
+    {
+        stateName: 'Chhattisgarh',
+        stateCode: 'CT'
+    },
+    {
+        stateName: '',
+        stateCode: ''
+    },
+    {
+        stateName: 'Dadra and Nagar Haveli',
+        stateCode: 'DN'
+    },
+    {
+        stateName: 'Daman and Diu',
+        stateCode: 'DD'
+    },
+    {
+        stateName: 'Delhi',
+        stateCode: 'DL'
+    },
+    {
+        stateName: 'Goa',
+        stateCode: 'GA'
+    },
+    {
+        stateName: 'Haryana',
+        stateCode: 'HR'
+    }   
+]
