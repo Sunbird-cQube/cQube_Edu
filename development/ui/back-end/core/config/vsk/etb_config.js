cd32c9b063dfb58360ac28e1bddcd23c698a5b91
atulmane01 <atul.mane@tibilsolutions.com>
2022-07-26 13:40:29 +0530

    added nista report for vsk

--- development/ui/back-end/core/config/vsk/etb_config.js
+++ development/ui/back-end/core/config/vsk/etb_config.js
@@ -1,4 +1,163 @@
 const dataSourceInfo = {
+    statesEnergizedTextBooks: {
+        map: {
+            mainFilter: 'State Code',
+            pathToFile: 'diksha_etb_program-started.json',
+            locations: [
+                {
+                    name: "Location",
+                    property: "State",
+                    level: "district",
+                    isState: true,
+                    tooltip: {
+                        name: "State/UT name"
+                    }
+                }
+            ],
+            dimensions: [
+                {
+                    name: "indicator",
+                    property: "Energised textbooks (State & NCERT adopted)",
+                    tooltip: {
+                        name: "Textbooks Energized"
+                    }
+                },
+            ],
+            filters: [],
+            options: {
+                legend: {
+                    title: 'Textbooks Energized'
+                }
+            }
+        },
+        loTable: {
+            mainFilter: 'State Code',
+            pathToFile: 'diksha_etb_etb-coverage.json',
+            gaugeChart: {
+                title: 'Overall ETB Coverage',
+                aggegration: {
+                    type: 'AVG',
+                    column: 'State energised (ETB)',
+                    against: "Total Physical textbooks excluding adopted (Curriculum+Supplementary)"
+                },
+                valueSuffix: "%"
+            },
+            columns: [
+                {
+                    name: "State/UT name",
+                    property: "State Name"
+                },
+                {
+                    name: "Total Curriculum Textbooks",
+                    property: "Total Physical textbooks excluding adopted (Curriculum+Supplementary)",
+                    class: "text-center"
+                },
+                {
+                    name: "Total Energized Textbooks",
+                    property: "Total Energised (Live ETB)",
+                    class: "text-center"
+                },
+                {
+                    name: "% Energized Textbooks",
+                    property: "ETB Coverage",
+                    isHeatMapRequired: true,
+				    color: '#002966'
+                }
+            ],
+            filters: []
+        }
+    },
+    qrCodeCoverageAcrossStates: {
+        map: {
+            pathToFile: 'diksha_etb_qr-coverage.json',
+            locations: [
+                {
+                    name: "Location",
+                    property: "State Name",
+                    level: "district",
+                    isState: true,
+                    tooltip: {
+                        name: "State/UT name"
+                    }
+                }
+            ],
+            dimensions: [
+                {
+                    name: "indicator",
+                    property: "QR Coverage",
+                    tooltip: {
+                        name: "QR Code Content Coverage"
+                    },
+                }
+            ],
+            filters: [],
+            options: {
+                legend: {
+                    title: 'QR Coverage'
+                },
+                tooltip: {
+                    reportTypeIndicator: 'percent'
+                }
+            }
+        },
+        barChart: {
+            pathToFile: 'diksha_etb_qr-coverage.json',
+            mainFilter: 'State Code',
+            gaugeChart: {
+                title: 'Overall QR Coverage',
+                aggegration: {
+                    type: 'AVG',
+                    column: 'QR covered',
+                    against: "Total QR Count"
+                },
+                valueSuffix: "%"
+            },
+            columns: [
+                {
+                    name: "State/UT name",
+                    property: "State Name",
+                    isLocationName: true
+                },
+                {
+                    name: "QR Coverage",
+                    property: "QR Coverage"
+                }
+            ],
+            filters: []
+        }
+    },
+    totalPlaysPerCapita: {
+        map: {
+            pathToFile: 'diksha_etb_plays-per-capita.json',
+            mainFilter: 'State Code',
+            locations: [
+                {
+                    name: "Location",
+                    property: "State Name",
+                    level: "district",
+                    isState: true,
+                    tooltip: {
+                        name: "State/UT name"
+                    }
+                }
+            ],
+            dimensions: [
+                {
+                    name: "indicator",
+                    property: "Plays per capita ( 1st April 2020)",
+                    tooltip: {
+                        name: "Learning Sessions per Capita"
+                    },
+                }
+            ],
+            filters: [],
+            options: {
+                legend: {
+                    title: 'Learning Sessions on Potential Users'
+                }
+            }
+        }
+    },
     totalLearningSessions: {
         barChart: {
             pathToFile: 'diksha_etb_learning-session.json',
