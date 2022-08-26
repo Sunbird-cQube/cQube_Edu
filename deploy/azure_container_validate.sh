adbc72f15f61929b218274af45ca1c130f9f4d36
asifbasha61 <asif.abdul@tibilsolutions.com>
2022-08-26 16:45:32 +0530

    azure and aws validation changes

--- deploy/azure_container_validate.sh
+++ deploy/azure_container_validate.sh
@@ -14,6 +14,26 @@ else
 fi
 }
 
+check_az_key(){
+    az_key_status=0
+    export AZURE_STORAGE_CONNECTION_STRING="$1"
+
+    az storage container list --account-name $azure_account_name --account-key $azure_account_key > /dev/null 2>&1
+    if [ ! $? -eq 0 ]; then echo "Error - Invalid az account key"; fail=1
+        az_key_status=1
+    fi
+}
+
+check_az_storage_account_name(){
+    az_account_status=0
+    export AZURE_STORAGE_CONNECTION_STRING="$1"
+
+    az storage container list --account-key $azure_account_key --account-name $azure_account_name > /dev/null 2>&1
+    if [ ! $? -eq 0 ]; then echo "Error - Invalid az account name"; fail=1
+        az_account_status=1
+    fi
+}
+
 check_az_storage_connection_string(){
     az_account_status=0
     export AZURE_STORAGE_CONNECTION_STRING="$1"
@@ -52,7 +72,7 @@ echo -e "\e[0;33m${bold}Validating the azure_container_config file...${normal}"
 
 
 # An array of mandatory values
-declare -a arr=("azure_input_container" "azure_output_container" "azure_storage_connection_string")
+declare -a arr=("azure_input_container" "azure_output_container" "azure_emission_container" "azure_account_name" "azure_account_key" "azure_storage_connection_string")
 
 # Create and empty array which will store the key and value pair from config file
 declare -A vals
@@ -79,6 +99,20 @@ case $key in
           check_az_storage_connection_string $key $value
        fi
        ;;
+   azure_account_name)
+       if [[ $value == "" ]]; then
+          echo "Error - in $key. Unable to get the value. Please check."; fail=1
+           else
+                   check_az_storage_account_name $key $value
+       fi
+       ;;
+   azure_account_key)
+       if [[ $value == "" ]]; then
+          echo "Error - in $key. Unable to get the value. Please check."; fail=1
+      else
+          check_az_key $key $value
+       fi
+       ;;
    azure_input_container)
        if [[ $value == "" ]]; then
           echo "Error - in $key. Unable to get the value. Please check."; fail=1
@@ -93,6 +127,14 @@ case $key in
           check_az_container $key $value
        fi
        ;;
+   azure_emission_container)
+       if [[ $value == "" ]]; then
+          echo "Error - in $key. Unable to get the value. Please check."; fail=1
+       else
+          check_az_container $key $value
+       fi
+       ;;
+
    *)
        if [[ $value == "" ]]; then
           echo -e "\e[0;31m${bold}Error - Value for $key cannot be empty. Please fill this value${normal}"; fail=1
