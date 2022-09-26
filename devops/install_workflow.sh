a6a899719a9b0ae0fc91283c4610c79658210841
asifbasha61 <asif.abdul@tibilsolutions.com>
2022-09-26 20:40:52 +0530

    Bug fixes and disabled otp configuration in keycloak

--- devops/install_workflow.sh
+++ devops/install_workflow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Workflow installation
+base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
 mode_of_installation=$(awk ''/^mode_of_installation:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
 if [[ $mode_of_installation == "localhost" ]]; then
 ansible-playbook ansible/install_workflow.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
