513d8134358b93b315653c248d1a520977d91cee
dhanush-2397 <dhanush.ks@tibilsolutions.com>
2022-10-18 16:49:16 +0530

    tool tip added and sidebar collapsed when selected any item

--- reference-visualization-app/dashboard/src/app/app.module.ts
+++ reference-visualization-app/dashboard/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -38,6 +39,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     ReactiveFormsModule,
     NgxSpinnerModule,
     CqubeLibraryModule,
+    MatTooltipModule,
     TranslateModule.forRoot({
       loader: {
           provide: TranslateLoader,
