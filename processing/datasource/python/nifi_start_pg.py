7d692b9628e05cacba5e9f0d77f9e1d82d5248f5
padmanabha1234 <padmanabha.j@tibilsolutions.com>
2022-10-20 14:21:39 +0530

    start pg issue is resolved

--- processing/datasource/python/nifi_start_pg.py
+++ processing/datasource/python/nifi_start_pg.py
@@ -1,17 +1,20 @@
 from deploy_nifi import get_nifi_root_pg, get_processor_group_info, rq, prop, logging, sys
 
 
-def start_processor_group(processor_group_name):
+def start_processor_group(processor_group_name, state):
+    header = {"Content-Type": "application/json"}
     pg_source = get_processor_group_info(processor_group_name)
     start_body = {"id": pg_source['component']['id'],
-                  "state": "RUNNING", "disconnectedNodeAcknowledged": False}
-    start_response = rq.put(f"{prop.NIFI_IP}:{prop.NIFI_PORT}/nifi-api/flow/process-groups/{pg_source['component']['id']}",
-                            json=start_body, headers=header)
+                  "state": state, "disconnectedNodeAcknowledged": False}
+    start_response = rq.put(
+        f"{prop.NIFI_IP}:{prop.NIFI_PORT}/nifi-api/flow/process-groups/{pg_source['component']['id']}",
+        json=start_body, headers=header)
     if start_response.status_code == 200:
-        logging.info(f"Successfuly scheduled {pg_source['component']['name']}")
+        logging.info(f"Successfully {state} {pg_source['component']['name']} Processor Group.")
         return True
     else:
         return start_response.text
+
 
 # Main.
 if __name__ == "__main__":
@@ -23,7 +26,8 @@ if __name__ == "__main__":
     """
     header = {"Content-Type": "application/json"}
     processor_group_name = sys.argv[1]
-    
-    # start telemetry processor group = cqube_telemetry_transformer
-    start_processor_group(processor_group_name)
+    state = sys.argv[2]
+
+    # enable/disable/start/stop the processor group
+    start_processor_group(processor_group_name, state)
 
